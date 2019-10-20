c9c4c86d50286c8a5a1cb444903ac8e4b22b879a
faisalsalhi <faisal.salhi@etu.univ-smb.fr>
2019-10-20 16:37:23 +0200

    Premier Commit

---
+++ src/com/company/arbreAVL.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class arbreAVL {
+    private static Integer initialBufferSize = 2000;
+    private static Integer indiceRacine = 1; // La racine de l'arbre commence à 1 dans notre représentation.
+    private Integer numberOfreAlloc = 1;
+    private Integer []arbre = new Integer[initialBufferSize];
+
+
+}
